c2a83774fe3bcb2c097006b180c1818805e30247
wcorona269 <wcorona269@gmail.com>
2022-09-09 18:10:12 -0400

    fix index issue in users table

--- db/migrate/20220503175356_update_users_with_cities.rb
+++ db/migrate/20220503175356_update_users_with_cities.rb
@@ -2,6 +2,6 @@ class UpdateUsersWithCities < ActiveRecord::Migration[5.2]
   def change
     rename_column :users, :display_name, :dname
     add_column :users, :city_id, :integer
-    add_index :users, :session_token
+    # add_index :users, :session_token
   end
 end
