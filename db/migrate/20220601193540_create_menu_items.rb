8993778ae1d3a46c9dbd28be445c24dd57a94410
wcorona269 <wcorona269@gmail.com>
2022-09-09 18:32:17 -0400

    update menus in db

--- db/migrate/20220601193540_create_menu_items.rb
+++ db/migrate/20220601193540_create_menu_items.rb
@@ -1,7 +1,5 @@
 class CreateMenuItems < ActiveRecord::Migration[5.2]
   def change
-    drop_table :menus
-    
     create_table :menu_items do |t|
       t.integer :rest_id, null: false, unique: true
       t.text :items, array: true, default: [], null: false
