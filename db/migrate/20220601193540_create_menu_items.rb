4dcf5b17835a5ec01091db09ec3af61655879f73
wcorona269 <wcorona269@gmail.com>
2022-09-19 21:40:55 -0400

    update migrations for delpoyment

--- db/migrate/20220601193540_create_menu_items.rb
+++ db/migrate/20220601193540_create_menu_items.rb
@@ -1,11 +1,10 @@
 class CreateMenuItems < ActiveRecord::Migration[5.2]
   def change
     create_table :menu_items do |t|
-      t.integer :rest_id, null: false, unique: true
+      t.integer :rest_id, null: false
       t.text :items, array: true, default: [], null: false
 
       t.timestamps
-      t.index :rest_id, unique: true
     end
   end
 end
